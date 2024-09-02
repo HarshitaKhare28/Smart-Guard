b0bd04daca18948cbd6900cc5c62dd7e6e1af41a
lalwanidj04 <147331633+lalwanidj04@users.noreply.github.com>
2024-09-02 15:10:21 +0530

    commit 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
     <Routes>
       <Route path="/signup"element={<Signup/>}/>
-      <Route path="/login"element={<Login/>}/>
+      <Route path="/"element={<Login/>}/>
     </Routes>
   </BrowserRouter>
     </>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,8 +7,20 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'custom-image': "url('/images/bg.jpg')",
+        'custom-image': "url('/images/bg1.jpg')",
+
       },
+      colors: {
+        'customGrey' : '#292a2c',
+        'customYellow' : '#dcba7b',
+        'customBlue' : '#5b79ad',
+        'customGrey1' : '#515255',
+      },
+      fontFamily: {
+        'inter' : ['Inter', 'sans-serif'],
+        'playfair': ['Playfair Display', 'serif'],
+        'font-weight': 400,
+      }
     },
   },
   plugins: [],
